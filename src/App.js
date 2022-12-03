ff1051dabf8a8ac244ff19ff9b8d735d4a132ac4
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-12-03 18:43:57 +0600

    last update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World! Hi Salman Azmi Rafi</h1>
+      <h1>I'm Salman Azmi Rafi </h1>
     </div>
   );
 }
